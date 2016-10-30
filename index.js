90ad54461bcbebc3ff0a2574f3c28471b3e3174f
appellation <nelson.will@live.com>
2016-10-30 00:28:22 -0700

    Queues, JOHN CENA

--- index.js
+++ index.js
@@ -5,35 +5,53 @@ const Discord = require('discord.js');
 const dotenv = require('dotenv').config({silent: true});
 const shuffle = require('knuth-shuffle').knuthShuffle;
 const fs = require('fs');
+const queue = require('queue');
 
 const client = new Discord.Client();
 
-let complete = true;
 client.on('message', msg => {
-    if(msg.content == '!plebhorn' && complete && msg.member.voiceChannel)  {
-        complete = false;
-        Promise.all([
-            new Promise((resolve, reject) => fs.readdir('audio', (err, success) => {
-                if(err) {
-                    reject(err);
-                }   else if(success.length == 0)    {
-                    reject();
-                }   else    {
-                    resolve(success);
-                }
-            })).then(shuffle),
-            msg.member.voiceChannel.join()
-        ]).then(resolutions => {
-            return resolutions[1].playFile('audio/' + resolutions[0][0]);
-        }).then(dispatcher => {
-            dispatcher.setVolume(0.5);
-            dispatcher.on('end', () => {
-                msg.member.voiceChannel.leave();
-                complete = true;
-            });
-        }).catch(err => {
-            console.error(err);
+    if(msg.content == '!plebhorn' && msg.member.voiceChannel)  {
+
+        let q;
+        if(msg.guild.queue) {
+            q = msg.guild.queue;
+        }   else    {
+            q = queue();
+            msg.guild.queue = q;
+        }
+
+        q.concurrency = 1;
+
+        q.once('end', () => {
+            msg.member.voiceChannel.leave();
+            msg.guild.queue_started = false;
         });
+
+        q.push((cb) => {
+            Promise.all([
+                new Promise((resolve, reject) => fs.readdir('audio', (err, success) => {
+                    if (err) {
+                        reject(err);
+                    } else if (success.length == 0) {
+                        reject();
+                    } else {
+                        resolve(success);
+                    }
+                })).then(shuffle),
+                msg.member.voiceChannel.join()
+            ]).then(resolutions => {
+                return resolutions[1].playFile('audio/' + resolutions[0][0]);
+            }).then(dispatcher => {
+                dispatcher.setVolume(0.5);
+                dispatcher.once('end', () => {
+                    cb();
+                });
+            }).catch(console.error)
+        });
+
+        if(!msg.guild.queue_started) {
+            q.start();
+        }
     }
 });
 
